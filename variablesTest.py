b6cce63f3f1f8bf7c155cffcafd9d0aa73f04383
stav <saprocontact@gmail.com>
2022-03-07 16:06:30 +0100

    Test opérateurs

--- variablesTest.py
+++ variablesTest.py
@@ -1,27 +1,45 @@
-nombre = 123
-print(nombre)
-# print(type(nombre))
-# exit()
+# nombre = 123
+# print(nombre)
+# # print(type(nombre))
+# # exit()
 
-nombre = 3.14
-print(nombre)
+# nombre = 3.14
+# print(nombre)
 
-is_day = True
-print(is_day)
+# is_day = True
+# print(is_day)
 
-has_sugar = False
-print(has_sugar)
+# has_sugar = False
+# print(has_sugar)
 
-html_code = '<h1>Titre blog</h1>'
-print(html_code)
+# html_code = '<h1>Titre blog</h1>'
+# print(html_code)
 
-nickname = "John \"Dead\" Doe"
-nickname = 'John "Dead" Doe'
+# nickname = "John \"Dead\" Doe"
+# nickname = 'John "Dead" Doe'
 
-#\n to end line
+# #\n to end line
 
-multiline_text = """foo
-bar
-baz"""
-print(multiline_text)
+# multiline_text = """foo
+# bar
+# baz"""
+# print(multiline_text)
 
+# verif que toutes les sources sont coupées
+# si c'est ok, print msg "vous pouvez partir en we"
+# sinon, print msg "il rest des sources à couper"
+
+# import random
+
+# electricity_is_off = bool(random.randint(0, 1))
+# water_is_off = bool(random.randint(0, 1))
+# gas_is_off = bool(random.randint(0, 1))
+
+# print("electricity_is_off", electricity_is_off)
+# print("water_is_off", water_is_off)
+# print("gas_is_off", gas_is_off)
+
+# if electricity_is_off and water_is_off and gas_is_off:
+#     print("Vous pouvez partir")
+# else:
+#     print("Il reste des sources à couper !")
