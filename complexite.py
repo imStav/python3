5dc6cfd3f673c80eaca32d5c7450cb1760cbb51b
stav <saprocontact@gmail.com>
2022-03-28 11:56:05 +0200

    Complexité algorithmique
    Cours sur la complexité (ou efficacité) d'un programme

--- complexite.py
+++ complexite.py
@@ -18,3 +18,56 @@ numbers = [123, 42, 3.14]
 for number in numbers:
     result = number * 2
     print(result)
+
+
+# O(n * n) = O(n ** 2)
+# n, est la quantité de données à traiter de la première liste
+# n, est la quantité de données à traiter de la deuxième liste
+numbers = [123, 42, 3.14]
+more_numbers = [2.71, 3.14, 0, 53]
+common_numbers = []
+
+for number in numbers:
+    for more_number in more_numbers:
+        if number == more_number:
+            common_numbers.append(number)
+
+
+# O(n * n) = O(n ** 2)
+matrix = [
+    [123, 42, 3.14],
+    [2.71, 3.14, 0],
+    [1, 2, 3]
+]
+
+for line in matrix:
+    for number in line:
+        result = number * 2
+        print(result)
+
+
+# O( n * n * n) = O(n3)
+cube = [
+    [
+        ['a1', 'a2', 'a3'],
+        ['a4', 'a5', 'a6'],
+        ['a7', 'a8', 'a9'],
+    ],
+
+    [
+        ['b1', 'b2', 'b3'],
+        ['b4', 'b5', 'b6'],
+        ['b7', 'b8', 'b9'],
+    ],
+
+    [
+        ['c1', 'c2', 'c3'],
+        ['c4', 'c5', 'c6'],
+        ['c7', 'c8', 'c9'],
+    ]
+]
+
+for square in cube:
+    for line in square:
+        for spot in line:
+            print(spot)
